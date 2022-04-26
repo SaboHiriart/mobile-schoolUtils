6be84c3fe34b5847bf67117bbd873df381bd549e
SaboHiriart <saboochoahiriart@outlook.com>
2022-04-26 17:50:06 -0500

    svg and showing

--- src/screens/LogInScreen/LogInScreen.js
+++ src/screens/LogInScreen/LogInScreen.js
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SvgUri } from 'react-native-svg';
+import Invest from '../../../assets/svg/wave.svg';
 
 const LogInScreen = () => {
   return (
     <View>
+      <Invest />
       <Text>LogInScreen</Text>
     </View>
   )
