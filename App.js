3befb879cf2a4f26a85199644fa7cdd5ee61c56b
SaboHiriart <saboochoahiriart@outlook.com>
2022-04-27 18:08:40 -0500

    Login View Frontend ready

--- App.js
+++ App.js
@@ -7,15 +7,22 @@
  */
 
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import LogInScreen from './src/screens/LogInScreen';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <LogInScreen />
-    </SafeAreaView>
+    <View style={{ width: '100%', height: '100%'}}>
+      <LogInScreen style={{ width: '100%', height: '100%'}}/>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default App;
--- src/screens/LogInScreen/LogInScreen.js
+++ src/screens/LogInScreen/LogInScreen.js
@@ -1,23 +1,91 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Alert} from 'react-native'
+import { Input, Icon, Button } from '@rneui/themed';
 import React from 'react'
-import Invest from '../../../assets/img/wave.png';
+import SVGWave from '../../../assets/svg/wave.svg';
 
 const LogInScreen = () => {
+  const createTwoButtonAlert = () =>
+    Alert.alert(
+      "Alert Title",
+      "My Alert Msg",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    );
+
   return (
-    <View style = {styles.container}>
-      <Image 
-        source={require('../../../assets/img/wave.png')}
-        style = {styles.wave}
+    <View style={styles.container}>
+      <SVGWave width={900} height={200} />
+      <Text style={styles.tittle}>TUM MOBILE</Text>
+      <Text style={styles.powered}>Powered by SaboHiriart</Text>
+      <View style={styles.inputContainer}>
+        <Input placeholder='Usuario' containerStyle={{ marginBottom: 15, }} leftIcon={
+          <Icon name='person' size={24} color='black' />
+        }
+        />
+        <Input placeholder='Contraseña' leftIcon={
+          <Icon name='lock' size={24} color='black' />
+        }
+          secureTextEntry={true} />
+      </View>
+
+      <Button
+        title="LOG IN"
+        buttonStyle={{
+          backgroundColor: '#000B76',
+          borderWidth: 2,
+          borderColor: 'white',
+          borderRadius: 30,
+        }}
+        containerStyle={{
+          width: 300,
+          marginHorizontal: 0,
+          marginVertical: 10,
+        }}
+        titleStyle={{ fontWeight: 'bold' }}
+        onPress={createTwoButtonAlert}
       />
+      <Text style={styles.singUp}>¿No tienes cuenta? <Text style={styles.register}>REGISTRATE</Text></Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: 100,
-    height:15
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
+  inputContainer: {
+    width: 300,
+    marginBottom: 50,
+  },
+  tittle: {
+    marginTop: -60,
+    marginBottom: 5,
+    fontSize: 50,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: "#000000",
+    fontFamily: 'Monserrat-Medium',
+  },
+  powered: {
+    marginBottom: 90,
+    textAlign: 'center',
+    fontFamily: 'Montserrat-Bold'
+  },
+  singUp: {
+    marginTop: 100,
+  },
+  register: {
+    color: '#000000',
+    fontWeight: 'bold'
+  }
 });
 
 export default LogInScreen
